515607242f8cba6c363477ac1ef05ae077ff2f38
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-10-02 19:18:34 +0200

    Update 4.2.c

--- 4.2.c
+++ 4.2.c
@@ -2,22 +2,5 @@
 
 int main() {
 
-	int a = 0;
-	int b = 0;
-	int result = 0;
 	
-	printf("Geben Sie ihre Basis ein!\n");
-	scanf("%d", &a);
-	printf("Geben Sie ihren Exponenten ein!\n");
-	scanf("%d", &b);
-
-	for (i = 0; i < b; i++) {
-		result *= a;
-	}
-	
-
-	printf("Das Ergebnis von %d hoch %d ist %d", a, b, result);
-	
-	return 0;
-
 }
