567989c5a8b71f2b00768c258ea9e2ef350fdc09
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-10-02 19:28:59 +0200

    Update 4.2.c

--- 4.2.c
+++ 4.2.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main() {
+int exp(int a, int b) {
+	int i = 1;
+	int c = a;
 
-	
+	for (i; i < b; i++) {
+		c = c * a;
+	}
+
+	return c;
+}
+
+int main() {
+	int x = 0;
+	int y = 0;
+
+	printf("Geben Sie ihre Basis ein!\n");
+	scanf("%d", &x);
+	printf("Geben Sie ihren Exponenten ein!\n");
+	scanf("%d", &y);
+	printf("\n%d hoch %d ist %d\n\n", x, y, exp(x, y));
+
+	return 0;
 }
