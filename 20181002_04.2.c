7be3809a0e142cc2c82b4086a8476547596436eb
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-10-03 19:37:27 +0200

    Update 20181002_04.2.c

--- 20181002_04.2.c
+++ 20181002_04.2.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int exp(int a, int b) {
-	int i = 1;
 	int c = a;
 
-	for (i; i < b; i++) {
+	for (int i = 1; i < b; i++) {
 		c = c * a;
 	}
 
@@ -19,7 +18,25 @@ int main() {
 	scanf("%d", &a);
 	printf("Geben Sie ihren Exponenten ein!\n");
 	scanf("%d", &b);
-	printf("%d hoch %d ist %d\n", a, b, exp(a, b));
+
+	if (b > 1) {
+		printf("%d hoch %d ist %d",a,b,exp(a, b));
+	}
+	else if (b < -1) {
+		b = -b;
+		float result = exp(a, b);
+		float endResult = 1 / result;
+
+		b = -b;
+		printf("%d hoch %d ist %f",a,b,endResult);
+	}
+	else if (b == 1) {
+		printf("%d hoch %d ist %d",a,b,a);
+	}
+	else {
+		printf("%d hoch %d ist 1",a,b);
+	}
+
 
 	return 0;
 }
