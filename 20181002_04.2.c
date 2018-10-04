1e02166b014ad0ed89bf7b5574ad5448c67ad185
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-10-04 19:05:34 +0200

    Update 20181002_04.2.c

--- 20181002_04.2.c
+++ 20181002_04.2.c
@@ -1,43 +1,41 @@
 #include <stdio.h>
 
-int exp(int a, int b) {
-	int c = a;
+int power(int base, int exponent) {
+	int result = base;
 
-	for (int i = 1; i < b; i++) {
-		c = c * a;
+	for (int i = 1; i < exponent; i++) {
+		result = result * base;
 	}
-
-	return c;
+	return result;
 }
 
 int main() {
-	int a = 0;
-	int b = 0;
+	int base = 0;
+	int exponent = 0;
 
 	printf("Geben Sie ihre Basis ein!\n");
-	scanf("%d", &a);
+	scanf("%d", &base);
 	printf("Geben Sie ihren Exponenten ein!\n");
-	scanf("%d", &b);
+	scanf("%d", &exponent);
 
-	if (b > 1) {
-		printf("%d hoch %d ist %d",a,b,exp(a, b));
+	if (exponent > 1) {
+		printf("%d hoch %d ist %d", base, exponent, power(base, exponent));
 	}
-	else if (b < -1) {
-		b = -b;
-		
-		float result = exp(a, b);
-		float endResult = 1 / result;
-
-		b = -b;
-		printf("%d hoch %d ist %f",a,b,endResult);
+	else if (exponent < -1) {
+		exponent = -exponent;
+		float tempResult = power(base, exponent);
+		float result = 1 / tempResult;
+		exponent = -exponent;
+		printf("%d hoch %d ist %f", base, exponent, result);
 	}
-	else if (b == 1) {
-		printf("%d hoch %d ist %d",a,b,a);
+	else if (exponent == 1) {
+		printf("%d hoch %d ist %d", base, exponent, base);
+	}
+	else if (base == 0) {
+		printf("%d hoch %d ist nicht definiert!", base, exponent);
 	}
 	else {
-		printf("%d hoch %d ist 1",a,b);
+		printf("%d hoch %d ist 1", base, exponent);
 	}
-
-
 	return 0;
 }
